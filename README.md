26c35e62c914893f53f2c562f59a3a63e1f53b0d
DevRooster <enriquemv937@gmail.com>
2025-01-22 21:11:14 -0500

    Gateway-Server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RS-CT 
 # ("Reservacion de Citas")
+# Config-Server port: 2025
+# Registry-Server port: 8025
+# Gateway-Server port: 8070
---
+++ gateway-server/src/main/java/dev/rooster/gateway_server/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package dev.rooster.gateway_server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class TokenDto {
+    private String token;    
+}
